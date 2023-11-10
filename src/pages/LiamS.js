967f3e6a60ad0464baf0ec0f7f77772fba4840c2
Realicide <143427454+Realicide@users.noreply.github.com>
2023-11-09 17:12:07 -0700

    Add files via upload
    Adding my page and updating the index.js

---
+++ src/pages/LiamS.js
@@ -0,0 +1,150 @@
+export default function examplePage() {
+    return (
+      <div className='container'>
+        <title>Website</title>
+        <link rel='icon' href='/favicon.ico' />
+
+        <main>
+          <div className='underline'>
+            <h1 className='title'>𓆏Frogs𓆏</h1>
+          </div>
+          <img
+            className='pic'
+            src='https://postimg.cc/bsw0FRHF'
+            alt='froigs'
+          />
+
+          <p className='description'>
+            Man I Love Frogs!
+            They're so dumb, there is nothing behind those eyes and I love it. Have you ever seen those videos of frogs jumping and failing miserably? Like they just faceplant?
+            Or those videos where frogs can't even catch a stationary meal? There is nothing going on in that head. Every frog in the world shares a braincell and one of them lost it.
+            They're also super cute and adorable. Most of them at least. Some are creepy. 
+          </p>
+
+          <p>
+                ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⠉⢳⣰⠋⠙⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢧⡀⠁⢀⡜⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠷⠋⠀⠀⠀⠀⠀⠀⣀⣀⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                ⠀⠀⠀⠀⠀⣠⣴⣶⢦⣦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡾⠟⠛⠛⠛⠿⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀
+                ⠀⠀⠀⠀⣼⠋⠀⠀⠀⠈⠻⣷⣄⠀⠀⠀⠀⠀⢀⢸⡏⠀⠀⢀⣀⣀⠀⠘⣷⠀⠀⠀⠀⠀⠀⠀⠀
+                ⠀⠀⠀⢸⡏⠀⠀⣠⣴⣦⡀⠸⣿⡷⠶⠾⠻⠛⠛⠃⠀⠀⡿⣿⣿⣿⠀⠀⣿⠀⠀⠀⠀⠀⠀⠀⠀
+                ⠀⠀⠀⣼⡇⠀⢸⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠿⠿⠛⠀⠀⠙⢷⡀⠀⠀⠀⠀⠀
+                ⠀⠀⠀⢽⡇⠀⠀⠉⠛⠛⠉⠀⠀⠀⠀⠰⣤⣤⠞⠀⠀⠀⠀⢀⢤⢤⠤⣄⠀⠀⢸⡇⠀⠀⠀⠀⠀
+                ⠀⠀⠀⢸⡇⠀⠀⢀⡴⢶⠲⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠳⢵⣁⡧⠃⠀⠀⣿⠃⠀⠀⠀⠀
+                ⠀⠀⠀⢸⣿⠀⠀⢯⣊⡧⠽⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⡇⠀⠀⠀⠀
+                ⠀⠀⠀⠐⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⡏⠀⠀⠀⠀
+                ⠀⢀⣠⣤⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣀⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠀⠙⠶⣄⠀⠀⠀
+                ⠀⣾⡏⠀⠸⡄⠀⠀⠀⠀⠀⠀⣰⠋⠁⠀⠀⠀⠀⠀⠀⠉⠳⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⡄⠀
+                ⠰⣿⠀⠀⠀⠁⠀⠀⠀⠀⠀⢰⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠀
+                ⠘⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠁
+                ⠀⢻⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠊⠆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡞⠁⠀⢠⠀⠀⠀⠀⠀⠀⠀⡟⠀
+                ⠀⠈⣿⡆⠀⠀⠀⠀⠘⢆⠀⠀⠀⢸⣆⠀⠀⠀⠀⠀⠀⠀⢀⣾⠀⠀⠀⣿⠀⠀⠀⠀⠀⠀⡼⠀⠀
+                ⠀⠀⠈⠻⣦⣀⠀⢀⠀⠈⣇⡄⠀⠀⠿⡆⠀⠀⠀⠀⠀⠀⣸⡇⠀⠀⠀⣿⠀⠀⠈⣶⡾⠟⠀⠀⠀
+                ⠀⠀⠀⠀⠈⢛⣻⡿⠀⠀⠈⣷⡀⠀⠀⢻⡦⠤⠤⠤⠤⠄⡇⠀⠀⠀⢸⡇⠀⠀⣾⣅⠀⠀⠀⠀⠀
+                ⠀⠀⠀⠀⠀⠸⣯⣦⣤⣤⣴⣼⣇⠀⠀⠀⣷⡀⠀⠀⠀⣸⡏⠀⢠⠀⢸⣷⣤⣤⣾⠏⠀⠀⠀⠀⠀
+                ⠀⠀⠀⠀⠀⠀⠈⠉⠉⠉⠉⠀⢹⣆⣷⣴⣾⡇⠀⠀⠀⢸⡇⢀⣼⠼⠾⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠿⠛⠃⠀⠀⠀⠀⠀⠀⠱⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+          </p>
+
+          <a className='button' href='/'>
+            <p>No more frogs :|</p>
+          </a>
+
+          <a className='frog_button' href='https://www.google.com/search?q=very+round+frog&tbm=isch&ved=2ahUKEwjIt9XHjriCAxWYz8kDHZljDkkQ2-cCegQIABAA&oq=very+round+frog&gs_lcp=CgNpbWcQAzIFCAAQgAQ6BAgjECc6BggAEAcQHjoGCAAQCBAeOggIABCxAxCDAToECAAQAzoICAAQgAQQsQM6BwgAEIoFEEM6CggAEIoFELEDEEM6CwgAEIAEELEDEIMBOg0IABCKBRCxAxCDARBDOgYIABAFEB46BwgAEBgQgARQqwtYyRhgqRloAHAAeACAAa8BiAGHCZIBBDE1LjGYAQCgAQGqAQtnd3Mtd2l6LWltZ8ABAQ&sclient=img&ei=93BNZYjgAZifp84Pmce5yAQ&bih=698&biw=1534&client=opera&hs=CVy#imgrc=5ZgPky2kbzapmM&imgdii=hXiRhP1lgIhoEM'>
+            <p>More frogs :D</p>
+          </a>
+        </main>
+
+        <style jsx>{`
+          .container {
+            min-height: 100vh;
+            padding: 0 0.5rem;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            max-width: 60rem;
+            margin: auto;
+          }
+
+          main {
+            padding: 5rem 0;
+            flex: 1;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+          }
+
+          .title {
+            margin: 0;
+            line-height: 1.15;
+            font-size: 4rem;
+          }
+
+          .underline {
+            /* border-bottom: solid black; */
+            margin-bottom: 1.5rem;
+          }
+
+          .description {
+            line-height: 1.5;
+            font-size: 1.5rem;
+          }
+
+          .pic {
+            height: 450px;
+            width: 450px;
+          }
+
+          .button {
+            /* margin: .5rem; */
+            flex-basis: 45%;
+            padding: 0.5rem;
+            text-align: left;
+            color: inherit;
+            text-decoration: none;
+            border: 1px solid #eaeaea;
+            border-radius: 10px;
+            transition: color 0.15s ease, border-color 0.15s ease;
+          }
+
+          .button:hover,
+          .button:focus,
+          .button:active {
+            color: #0070f3;
+            border-color: #0070f3;
+          }
+
+          .button p {
+            font-size: 1rem;
+            font-weight: 600;
+          }
+
+          code {
+            background: #fafafa;
+            border-radius: 5px;
+            padding: 0.75rem;
+            font-size: 1.1rem;
+            font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+              DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+          }
+        `}</style>
+
+        <style jsx global>{`
+          html,
+          body {
+            padding: 0;
+            margin: 0;
+            font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+              Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+              sans-serif;
+          }
+
+          * {
+            box-sizing: border-box;
+          }
+        `}</style>
+      </div>
+    );
+}
